dbcad4a2300b53bb7179220976d8c14c698bbb23
Aadigoel <64415203+Aadigoel@users.noreply.github.com>
2023-04-27 22:00:37 +0530

    Update 1.1 copy.bat

--- copy.bat
+++ copy.bat
@@ -1,5 +1,5 @@
 copy ~\Workspaces\upshop\uc1\.env \var\lib\jenkins\workspace\upshop\uc1
-copy ~\Workspaces\upshop\uc2\.env \var\lib\jenkins\workspace\upshop\uc1
-copy ~\Workspaces\upshop\uc3\.env \var\lib\jenkins\workspace\upshop\uc1
+copy ~\Workspaces\upshop\uc2\.env \var\lib\jenkins\workspace\upshop\uc2
+copy ~\Workspaces\upshop\uc3\.env \var\lib\jenkins\workspace\upshop\uc3
 
 
